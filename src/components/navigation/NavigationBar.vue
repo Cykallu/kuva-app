06810a6e27010d12963e3b0ad2afc6e6832e7e21
AleYK <100762788+Cykallu@users.noreply.github.com>
2022-04-11 21:17:01 +0300

    Update NavigationBar.vue

--- src/components/navigation/NavigationBar.vue
+++ src/components/navigation/NavigationBar.vue
@@ -27,9 +27,24 @@ provide('showLoginView', showLoginView)
     position: fixed;
     width: 100%;
     top: 0px;
+    padding: 10px;
+    background-color: rgb(195, 209, 203);
 }
 a {
     margin: 20px;
     font-size: 18px;
+    font-family: sans-serif;
+    text-decoration: none;
+}
+a:visited{
+    color: rgb(0, 0, 0);
+}
+a:hover,
+a:active {
+  background-color: rgb(151, 163, 158);
+  cursor: pointer;
+  border: 1px;
+    border-color: black;
+    border-style: groove;
 }
 </style>
