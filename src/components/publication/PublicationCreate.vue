31051224aad8adfef9f6f3e07c4675316a6c4d4b
AleYK <100762788+Cykallu@users.noreply.github.com>
2022-04-20 14:33:35 +0300

    fixed create

--- src/components/publication/PublicationCreate.vue
+++ src/components/publication/PublicationCreate.vue
@@ -1,9 +1,6 @@
-<script>
+<script setup>
 import { computed, reactive, unref } from 'vue';
 import { publicationService } from '../../services/publicationService';
-import { router } from '../../router'
-
-//julkaisun datan määritys
 const publicationData = reactive({
     title: '',
     description: '',
@@ -11,68 +8,55 @@ const publicationData = reactive({
     visibility: 2, // tarkoittaa julkista postausta
     tags: []
 })
-//Datan Validointi
-//const GoodExtensions = ['jpg','jpeg','gif','png','webp']
 const isDataValid = computed(() => {
     const urlValidation = publicationData.url.includes('https://')
-    const oldUrlValidation = publicationData.url.includes('http://')
-    const descriptionValidation = publicationData.description.length < 1000
+    const descriptionValidation = publicationData.description.length < 1000 && publicationData.description.length > 0
     const titleValidation = publicationData.title.length > 2
-    /*let parts = publicationData.url.split(".")
-    let extension = parts[parts.length - 1]
-    let extensionValidation = false
-    if (goodExtensions.includes(extension)) {
-        extensionValidation = true
-    }*/
-    // const urlValidation = (newUrlValidation || oldUrlValidation) && extensionValidation
     return {
-        urlValidation: urlValidation ? "OK" : "Vain https-osoitteet sallittu",
-        descriptionValidation: descriptionValidation ? "OK" : "Liian pitkä kuvaus",
-        titleValidation: titleValidation ? "OK" : "Liian lyhyt otsikko",
-        isAllValid: urlValidation && descriptionValidation && titleValidation && oldUrlValidation 
+        urlValidation: urlValidation ? 'OK' : 'Vain https osoitteet ovat sallittu',
+        descriptionValidation: descriptionValidation ? 'OK' : 'Kuvauksen teksti on liian pitkä',
+        titleValidation: titleValidation ? 'OK' : 'Otsikon täytyy olla ainakin kolme merkkiä pitkä',
+        isAllValid: urlValidation && descriptionValidation && titleValidation
     }
 })
-//Uuden julkaisun luonti
 const createNewPublication = async () => {
     if (!isDataValid.value.isAllValid) return
     const { data, error } = await publicationService.usePost(publicationData)
-    //Tarkistetaan datan lähetys ja virheiden tarkistus
-    //Jos kaikki OK niin tyhjennetään kentät
     if (data.value && !error.value) {
         publicationData.title = ''
         publicationData.description = ''
         publicationData.url = ''
-        router.push({ path: "/" })
     }
 }
-
-
 </script>
 
 <template>
     <div class="publication-form">
-        <label> otsikko </label>
-        <small class="errorMessage" >{{isDataValid.titleValidation}}</small>
-        <input v-model="publicationData.title" type="text">
-        <label> kuvaus </label>
-        <small class="errorMessage" >{{isDataValid.descriptionValidation}}</small>
-        <input v-model="publicationData.description" type="text">
-        <label> URL </label>
-        <small class="errorMessage" >{{isDataValid.urlValidation}}</small>
-        <input v-model="publicationData.url" type="text">
+        <label>Otsikko</label>
+        <input v-model="publicationData.title" type="text" />
+
+        <small>{{ isDataValid.titleValidation }}</small>
+        <label>Kuvaus</label>
+        <input v-model="publicationData.description" type="text" />
+
+        <small>{{ isDataValid.descriptionValidation }}</small>
+        <label>URL</label>
+        <input v-model="publicationData.url" type="text" />
+
+        <small>{{ isDataValid.urlValidation }}</small>
+        <button :disabled="!isDataValid.isAllValid" @click="createNewPublication">Lähetä</button>
     </div>
-    
 </template>
 
-<style>
-.errorMessage{
-    color: red;
-}
+<style scoped>
 .publication-form {
     display: flex;
     flex-direction: column;
     height: 200px;
     justify-content: space-between;
     width: 200px;
+}
+.button:active{
+    background-color: green;
 }
 </style>
